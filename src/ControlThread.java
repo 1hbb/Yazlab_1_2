858d856e7f77e288141a8f313bf6a27705d51168
burk burs <burkburs@burks-MacBook-Pro.local>
2020-12-09 17:00:52 +0300

    Add color to terminal output

--- src/ControlThread.java
+++ src/ControlThread.java
@@ -1,37 +1,4 @@
 public class ControlThread extends Thread {
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    public static final String RESET_COLOR = "\033[0m";
-
-    public void printFloorInfos() {
-        System.out.println("All queue count: " + App.ALL_QUEUE);
-        System.out.println("0. floor: queue: " + App.FLOOR_0_QUEUE_COUNT + " array: " + App.FLOOR_0_QUEUE);
-        System.out.println("1. floor:" + "all:" + App.FLOOR_1_ALL + " queue: " + App.FLOOR_1_QUEUE_COUNT + " array: "
-                + App.FLOOR_1_QUEUE);
-        System.out.println("2. floor:" + "all:" + App.FLOOR_2_ALL + " queue: " + App.FLOOR_2_QUEUE_COUNT + " array: "
-                + App.FLOOR_2_QUEUE);
-        System.out.println("3. floor:" + "all:" + App.FLOOR_3_ALL + " queue: " + App.FLOOR_3_QUEUE_COUNT + " array: "
-                + App.FLOOR_3_QUEUE);
-        System.out.println("4. floor:" + "all:" + App.FLOOR_4_ALL + " queue: " + App.FLOOR_4_QUEUE_COUNT + " array: "
-                + App.FLOOR_4_QUEUE);
-        System.out.println();
-
-    }
-
-    public void printElevatorInfos(Elevator elevator) {
-        if (elevator.MODE == "working") {
-            System.out.println(ANSI_GREEN_BACKGROUND + "mode: " + elevator.MODE + RESET_COLOR);
-        } else {
-            System.out.println(ANSI_RED_BACKGROUND + "mode: " + elevator.MODE + RESET_COLOR);
-        }
-        System.out.println("floor: " + elevator.FLOOR);
-        System.out.println("destination: " + elevator.DESTINATION);
-        System.out.println("direction: " + elevator.DIRECTION);
-        System.out.println("capacity: " + elevator.ELEVATOR_CAPACITY);
-        System.out.println("count inside: " + elevator.COUNT_INSIDE);
-        System.out.println("inside: " + elevator.INSIDE);
-        System.out.println(RESET_COLOR);
-    }
 
     @Override
     public void run() {
@@ -58,8 +25,8 @@ public class ControlThread extends Thread {
         elevator5.start();
 
         while (true) {
-            printFloorInfos();
-            printElevatorInfos(elevator1);
+            ShowInfos.printFloorInfos();
+            ShowInfos.printElevatorInfos(elevator1);
 
             if (App.ALL_QUEUE > 20 && App.ALL_QUEUE < 40) {
                 elevator2.setMode("working");
@@ -87,10 +54,10 @@ public class ControlThread extends Thread {
                 elevator5.setMode("idle");
             }
 
-            printElevatorInfos(elevator2);
-            printElevatorInfos(elevator3);
-            printElevatorInfos(elevator4);
-            printElevatorInfos(elevator5);
+            ShowInfos.printElevatorInfos(elevator2);
+            ShowInfos.printElevatorInfos(elevator3);
+            ShowInfos.printElevatorInfos(elevator4);
+            ShowInfos.printElevatorInfos(elevator5);
 
             try {
                 Thread.sleep(500);
---
+++ src/ShowInfos.java
@@ -0,0 +1,36 @@
+public class ShowInfos {
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String RESET_COLOR = "\033[0m";
+
+    public static void printFloorInfos() {
+        System.out.println("All queue count: " + App.ALL_QUEUE);
+        System.out.println("0. floor: queue: " + App.FLOOR_0_QUEUE_COUNT + " queue array: " + App.FLOOR_0_QUEUE);
+        System.out.println("1. floor:" + "all:" + App.FLOOR_1_ALL + " queue: " + App.FLOOR_1_QUEUE_COUNT
+                + " queue array: " + App.FLOOR_1_QUEUE);
+        System.out.println("2. floor:" + "all:" + App.FLOOR_2_ALL + " queue: " + App.FLOOR_2_QUEUE_COUNT
+                + " queue array: " + App.FLOOR_2_QUEUE);
+        System.out.println("3. floor:" + "all:" + App.FLOOR_3_ALL + " queue: " + App.FLOOR_3_QUEUE_COUNT
+                + " queue array: " + App.FLOOR_3_QUEUE);
+        System.out.println("4. floor:" + "all:" + App.FLOOR_4_ALL + " queue: " + App.FLOOR_4_QUEUE_COUNT
+                + " queue array: " + App.FLOOR_4_QUEUE);
+        System.out.println();
+
+    }
+
+    public static void printElevatorInfos(Elevator elevator) {
+        System.out.println(elevator.getName().toUpperCase());
+        if (elevator.MODE == "working") {
+            System.out.println(ANSI_GREEN_BACKGROUND + "mode: " + elevator.MODE + RESET_COLOR);
+        } else {
+            System.out.println(ANSI_RED_BACKGROUND + "mode: " + elevator.MODE + RESET_COLOR);
+        }
+        System.out.println("floor: " + elevator.FLOOR);
+        System.out.println("destination: " + elevator.DESTINATION);
+        System.out.println("direction: " + elevator.DIRECTION);
+        System.out.println("capacity: " + elevator.ELEVATOR_CAPACITY);
+        System.out.println("count inside: " + elevator.COUNT_INSIDE);
+        System.out.println("inside: " + elevator.INSIDE);
+        System.out.println(RESET_COLOR);
+    }
+}
